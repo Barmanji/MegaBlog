23369cc7dfbca45facc0a38ad72a6d7615ae3ac4
Barmanji <barmanjiaj@gmail.com>
2024-10-04 09:14:46 +0530

    updated RTE and many components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,28 @@
-import Footer from "./Footer/Footer";
+import Select from "./Select";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-export { Footer, Header, logo, Container, LogoutBtn }
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+	Header,
+	Footer,
+	Container,
+	Logo,
+	LogoutBtn,
+	RTE,
+	Signup,
+	Login,
+	Button,
+	PostCard,
+	AuthLayout,
+	Input,
+	Select,
+}
